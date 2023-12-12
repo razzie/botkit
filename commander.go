a610826d9217a72b3c88199896abf7648b01e9b6
Gábor Görzsöny <gabor@gorzsony.com>
2023-12-13 00:02:05 +0100

    Added context support for commands

--- commander.go
+++ commander.go
@@ -1,76 +1,96 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"reflect"
 	"strings"
 )
 
+type Command func(context.Context, string) ([]any, error)
+
 type Commander struct {
-	cmds map[string]func(string) ([]any, error)
+	cmds map[string]Command
 }
 
 func NewCommander() *Commander {
 	return &Commander{
-		cmds: make(map[string]func(string) ([]any, error)),
+		cmds: make(map[string]Command),
 	}
 }
 
-func (cmdr *Commander) RegisterCommand(cmd string, fn reflect.Value) error {
+func (cmdr *Commander) RegisterCommand(cmd string, callback any) error {
+	fn := reflect.ValueOf(callback)
 	if fn.Kind() != reflect.Func {
 		return fmt.Errorf("not a function")
 	}
-	cmdr.cmds[cmd] = func(line string) (results []any, err error) {
+	cmdr.cmds[cmd] = func(ctx context.Context, line string) (results []any, err error) {
 		defer func() {
 			if r := recover(); r != nil {
-				err = fmt.Errorf("%v", r)
+				err = fmt.Errorf("panic: %v", r)
 			}
 		}()
 		args := strings.Fields(line)
-		return callFunction(fn, args)
+		return callFunction(ctx, fn, args)
 	}
 	return nil
 }
 
-func (cmdr *Commander) Call(cmd string, args string) ([]any, error) {
+func (cmdr *Commander) Call(ctx context.Context, cmd string, args string) ([]any, error) {
 	fn, ok := cmdr.cmds[cmd]
 	if !ok {
 		return nil, fmt.Errorf("unknown command: %s", cmd)
 	}
-	return fn(args)
+	return fn(ctx, args)
 }
 
-func callFunction(fn reflect.Value, args []string) (results []any, err error) {
+func callFunction(ctx context.Context, fn reflect.Value, args []string) ([]any, error) {
 	fnType := fn.Type()
-	if fnType.NumIn() != len(args) {
-		err = fmt.Errorf("expected %d arguments, got %d", fnType.NumIn(), len(args))
-		return
+	inputs, err := convertInputs(ctx, fnType, args)
+	if err != nil {
+		return nil, err
 	}
-
-	inputs := make([]reflect.Value, len(args))
-	for i, arg := range args {
-		paramType := fnType.In(i)
-		val, convErr := convertToType(arg, paramType)
-		if convErr != nil {
-			err = fmt.Errorf("error converting argument %d: %s", i, convErr)
-			return
-		}
-		inputs[i] = val
+	outputs := make([]any, 0, fnType.NumOut())
+	for _, o := range fn.Call(inputs) {
+		outputs = append(outputs, o.Interface())
 	}
-
-	results = make([]any, 0, fnType.NumOut())
-	for _, r := range fn.Call(inputs) {
-		results = append(results, r.Interface())
-	}
-
-	if len(results) > 0 {
-		last := results[len(results)-1]
+	if len(outputs) > 0 {
+		last := outputs[len(outputs)-1]
 		if e, ok := last.(error); ok {
 			err = e
 		}
 	}
+	return outputs, err
+}
 
-	return
+func convertInputs(ctx context.Context, fnType reflect.Type, args []string) ([]reflect.Value, error) {
+	numIn := fnType.NumIn()
+	if numIn == 0 {
+		return nil, nil
+	}
+	inputs := make([]reflect.Value, numIn)
+	skipped := 0
+
+	if fnType.In(0).Implements(reflect.TypeOf((*context.Context)(nil)).Elem()) {
+		inputs[0] = reflect.ValueOf(ctx)
+		skipped++
+	}
+	if numIn-skipped != len(args) {
+		return nil, fmt.Errorf("expected %d argument(s), got %d", numIn-skipped, len(args))
+	}
+
+	for i, arg := range args {
+		paramType := fnType.In(i + skipped)
+		val, convErr := convertToType(arg, paramType)
+		if convErr != nil {
+			return nil, fmt.Errorf("error converting argument %d: %s", i, convErr)
+		}
+		inputs[i+skipped] = val
+	}
+
+	// TODO: variadic support
+
+	return inputs, nil
 }
 
 func convertToType(value string, targetType reflect.Type) (reflect.Value, error) {
--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"context"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+type ctxKey string
+
+var ctxMessage ctxKey = "ctxMessage"
+
+func injectMessage(ctx context.Context, msg *tgbotapi.Message) context.Context {
+	return context.WithValue(ctx, ctxMessage, msg)
+}
+
+func getMessage(ctx context.Context) *tgbotapi.Message {
+	if msg, ok := ctx.Value(ctxMessage).(*tgbotapi.Message); ok {
+		return msg
+	}
+	return nil
+}
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		panic(err)
 	}
+
+	send := func(ctx context.Context, msg string) {
+		if _, err := bot.Send(tgbotapi.NewMessage(getMessage(ctx).Chat.ID, msg)); err != nil {
+			panic(err)
+		}
+	}
+
+	cmdr := NewCommander()
+	cmdr.RegisterCommand("send", send)
 
 	numericKeyboard := tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
@@ -21,13 +47,28 @@ func main() {
 	updateConfig := tgbotapi.NewUpdate(0)
 	updateConfig.Timeout = 30
 	updates := bot.GetUpdatesChan(updateConfig)
+
 	for update := range updates {
 
 		if update.Message != nil {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyMarkup = numericKeyboard
-			if _, err := bot.Send(msg); err != nil {
-				panic(err)
+			if update.Message.IsCommand() {
+				ctx := injectMessage(context.Background(), update.Message)
+				_, err := cmdr.Call(ctx, update.Message.Command(), update.Message.CommandArguments())
+				if err != nil {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, err.Error())
+					msg.ReplyToMessageID = update.Message.MessageID
+					if _, err := bot.Send(msg); err != nil {
+						panic(err)
+					}
+				}
+
+			} else {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+				msg.ReplyToMessageID = update.Message.MessageID
+				msg.ReplyMarkup = numericKeyboard
+				if _, err := bot.Send(msg); err != nil {
+					panic(err)
+				}
 			}
 		}
 
